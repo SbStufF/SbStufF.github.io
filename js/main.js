5e0c8ff877735e416e08a5e65e49cd5bc8fa2da0
SbStufF <50555463+SbStufF@users.noreply.github.com>
2019-05-13 21:37:32 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$( function () {
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+    
+    			// grab the initial top offset of the navigation 
+		   	var stickyNavTop = $('.head').offset().top;
+		   	
+		   	// our function that decides weather the navigation bar should have "fixed" css position or not.
+		   	var stickyNav = function(){
+			    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+			         
+			    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+			    // otherwise change it back to relative
+			    if (scrollTop > stickyNavTop) { 
+			        $('.head').addClass('sticky');
+			    } else {
+			        $('.head').removeClass('sticky'); 
+			    }
+			};
+
+			stickyNav();
+			// and run it again every time you scroll
+			$(window).scroll(function() {
+				stickyNav();
+			});
+    
+         
+});
